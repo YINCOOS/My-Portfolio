f8ba03e0300cc5724e4f63dc4fc835062c803b90
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-07 12:48:56 +0100

    successfully rendered the landing page, now i'm working on the social media handles link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import ME from '../../Assets/me.png'
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+
+       <header>
+        <div className="container header__container">
+            <h5>Hello I'm</h5>
+            <h1>Adeyinka</h1>
+            <h5 className='text-light'>MERN STACK DEVELOPER</h5>
+            <CTA />
+
+            <div className='me'>
+                <img src={ME} alt='Adeyinka' />
+            </div>
+
+            <a href='#Contact' className='scroll__down'>Scrool Down</a>
+        </div>
+    </header>
+    
   )
 }
 
