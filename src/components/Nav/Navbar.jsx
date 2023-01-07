8bf496b73f2be3ae4c5297e0b474f998cab44fa0
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-07 15:03:51 +0100

    added active button to each button clicked and remains active so long you're still on it else it changes to the activeness to the next button clicked

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Navbar.css'
 import {FcHome} from 'react-icons/fc'
@@ -5,16 +6,18 @@ import {FaUserAlt} from 'react-icons/fa'
 import {BiBookBookmark} from 'react-icons/bi'
 import {MdOutlineHomeRepairService} from 'react-icons/md'
 import {BiMessageDetail} from 'react-icons/bi'
+import { useState } from 'react'
 
 
 const Navbar = () => {
+    const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-        <a href='#' className='active'><FcHome /></a>
-        <a href='#About'><FaUserAlt /></a>
-        <a href='#Experience'><BiBookBookmark /></a>
-        <a href='#Services'><MdOutlineHomeRepairService /></a>
-        <a href='#Contact'><BiMessageDetail /></a>
+        <a href='#' onClick={()=> setActiveNav('#')}className={activeNav === '#' ? 'active' : ''}><FcHome /></a>
+        <a href='#About' onClick={()=> setActiveNav('#About')} className={activeNav === '#About' ? 'active' : ''}><FaUserAlt /></a>
+        <a href='#Experience' onClick={()=> setActiveNav('#Experience')} className={activeNav === '#Experience' ? 'active' : ''}><BiBookBookmark /></a>
+        <a href='#Services' onClick={()=> setActiveNav('#Services')} className={activeNav === '#Services' ? 'active' : ''}><MdOutlineHomeRepairService /></a>
+        <a href='#Contact' onClick={()=> setActiveNav('#Contact')} className={activeNav === '#Contact' ? 'active' : ''}><BiMessageDetail /></a>
     </nav>
   )
 }
