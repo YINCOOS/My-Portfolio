c0729f1c29c059e5c2d32e8cbc7d58081cae7dee
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-08 00:36:16 +0100

    Completed the About & Experience page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,48 @@
 import React from 'react'
 import './About.css'
+import ME from '../../Assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {AiOutlineUsergroupDelete} from 'react-icons/ai'
+import {ImFolderOpen} from 'react-icons/im'
 const About = () => {
   return (
-   <section id='About'>Remember me oh lord</section>
+   <section id='About'>
+    <h5>Get to Know</h5>
+    <h2>About Me</h2>
+
+    <div className='container about__container'>
+        <div className='about__me'>
+            <div className='about__me-image'>
+                <img src={ME} alt='Adeyinka' />
+            </div>
+        </div>
+        <div className='about__content'>
+            <div className='about__cards'>
+                <article className='about__card'>
+                    <FaAward  className='about__icon'/>
+                    <h5>Experience</h5>
+                    <small>3+ Years Working Experience</small>
+                </article>
+
+                <article className='about__card'>
+                <AiOutlineUsergroupDelete  className='about__icon'/>
+                <h5>Clients</h5>
+                <small>200+ Worlwide</small>
+            </article>
+
+            <article className='about__card'>
+                <ImFolderOpen className='about__icon'/>
+                <h5>Projects</h5>
+                <small>80+ completed Projects</small>
+            </article>
+            </div>
+            <p>
+            I am a highly experienced MERN stack developer with a proven track record of developing dynamic, user-friendly web applications and websites. I have extensive experience working with the MongoDB, Express, React, and Node.js (MERN) technologies. I have a strong understanding of front-end technologies such as HTML5, CSS3, and JavaScript, as well as back-end technologies such as Node.js and Express.
+            </p>
+            <a href='#Contact' className='btn btn-primary'>Contact Me</a>
+        </div>
+    </div>
+   </section>
   )
 }
 
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,9 +1,96 @@
 import React from 'react'
 import './Experience.css'
-
+import {HiShieldCheck} from 'react-icons/hi'
 const Experience = () => {
   return (
-    <section id="Experience">Experience De Difference</section>
+    <section id="Experience">
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+
+        <div className='container experience__container'>
+            <div className='experience__frontend'>
+                <h3>Frontend Development</h3>
+                <div className='experience__content'>
+                <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>HTML</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                        
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>CSS</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>Reactjs</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>JavaScript</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>Bootstrap</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                </div>
+            </div>
+            <div className='experience__backend'>
+                <h3>Backend Development</h3>
+                <div className='experience__content'>
+                <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>Node js</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>Express</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>MongoDB</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>MySQL</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                    <article className='experience__details'>
+                        <HiShieldCheck className='experience__details-icon'/>
+                        <div>
+                            <h4>jQuery</h4>
+                            <small className='text-light'>Experienced</small>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </section>
   )
 }
 
