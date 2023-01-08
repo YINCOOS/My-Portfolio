abb0ec29c5f3e9109580e0dce949d2c95715b410
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-08 18:16:09 +0100

    Completed the portfolio section by generating the data and populating on the frontend dynamically not hard-coded

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,64 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../Assets/portfolio1.webp'
+import IMG2 from '../../Assets/portfolio2.webp'
+import IMG3 from '../../Assets/portfolio3.webp'
+
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Book a ride App',
+        github: 'https://github.com/YINCOOS/Ride-App',
+        demo: 'https://github.com/YINCOOS/Ride-App'
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'A Discount Landing-Page',
+        github: 'https://github.com/YINCOOS/DISCOUNT-LANDING-PAGE',
+        demo: 'https://github.com/YINCOOS/DISCOUNT-LANDING-PAGE'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'User Registration API',
+        github: 'https://github.com/YINCOOS/DISCOUNT-LANDING-PAGE',
+        demo: 'https://github.com/YINCOOS/DISCOUNT-LANDING-PAGE'
+    },
+  
+]
+
 const Portfolio = () => {
   return (
-    <section id="Portfolio">Portfolio inclusive</section>
+    <section id="Portfolio">
+        <h5>My Recent Work</h5>
+        <h2>Portfolio</h2>
+
+        <div className='container portfolio__container'>
+            {
+                data.map(({id, image, title, github, demo})=>{
+                    return (
+                <article key={id} className='portfolio__item'>
+                        <div className="portfolio__item-image">
+
+                            <img src={image} alt={title}/>
+                        </div>
+                            <h3>{title}</h3>
+                        <div className='portfolio__item-cta'>
+                            <a href={github} className='btn' target='_blank'>Github</a>
+                            <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                        </div>
+                </article>
+                    )
+                })
+            }
+
+            
+
+        </div>
+    </section>
   )
 }
 
