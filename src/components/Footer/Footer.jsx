ef437b8557f87c832cbdf862f36dcc056fef6a26
Decagon <decagon@Decagons-MacBook-Air.local>
2023-01-09 02:01:57 +0100

    finished the footer which brought me close to the end of the project, I can now proceed to deploy my portfolio now. Yaaaay

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,40 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Footer.css'
+import {ImLinkedin} from 'react-icons/im'
+import {BsGithub} from 'react-icons/bs'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {FaTelegramPlane} from 'react-icons/fa'
+import {BsWhatsapp} from 'react-icons/bs'
+
+
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href='#' className='footer__logo'>Adeyinka</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#About'>About</a></li>
+        <li><a href='#Experience'>Experience</a></li>
+        <li><a href='#Services'>Service</a></li>
+        <li><a href='#Portfolio'>Portfolio</a></li>
+        <li><a href='#Testimonial'>Testimonial</a></li>
+        <li><a href='#Contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://github.com/YINCOOS'><BsGithub/></a>
+        <a href='https://www.linkedin.com/in/ogunsemoyin-david-7a32051ba/'><ImLinkedin/></a>
+        <a href='https://msng.link/o/?07032248112=wa'><BsWhatsapp/></a>
+        <a href='https://twitter.com/yincoos/'><AiOutlineTwitter/></a>
+        <a href='https://msng.link/o/?YINCOOS=tg'><FaTelegramPlane/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Yincoos_World. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
