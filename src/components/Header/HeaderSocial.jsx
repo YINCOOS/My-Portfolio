756538150320b1db9059c96c543a2ae44374e9c0
Samson Osiomwan <81101034+samsonosiomwan@users.noreply.github.com>
2023-04-30 21:59:07 +0100

    added new linkedn link

--- src/components/Header/HeaderSocial.jsx
+++ src/components/Header/HeaderSocial.jsx
@@ -6,7 +6,7 @@ import {AiOutlineTwitter} from 'react-icons/ai'
 const HeaderSocial = () => {
   return (
     <div className='header__socials'>
-      <a href='https://www.linkedin.com/in/ogunsemoyin-david-7a32051ba/' target='_blank' rel="noreferrer"><ImLinkedin/></a>
+      <a href='https://www.linkedin.com/in/adeyinka-david-ogunsemoyin-7a32051ba/' target='_blank' rel="noreferrer"><ImLinkedin/></a>
       <a href='https://github.com/YINCOOS/' target='_blank' rel="noreferrer"><BsGithub/></a>
       <a href='https://twitter.com/yincoos/' target='_blank' rel="noreferrer"><AiOutlineTwitter/></a>
     </div>
